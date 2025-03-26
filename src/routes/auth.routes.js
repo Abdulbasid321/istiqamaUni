82a49cabbee69a945c4c97d4842f357398a0151c
abdulbasid <abdulbasidhussain9@gmail.com>
2025-03-26 17:25:40 +0100

    upsadte

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,11 +2,11 @@ const { Router } = require('express');
 
 
 const router = Router();
-// const userController = require('../controllers/user.controller')
+const userController = require('../controllers/user.controller')
 const studentController = require('../controllers/student.controller')
 router.post('/login', studentController.login)
 // router.post('/login', userController.login)
-// router.post('/admin', userController.adminLogin)
+router.post('/admin', userController.adminLogin)
 
 
 
